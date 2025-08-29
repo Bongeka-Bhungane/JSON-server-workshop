1ef6946f68cefda266122d2f89159edefe904792
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-08-29 09:59:57 +0200

    feat:displayed one set of data

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
-// import axios from "axios";
-
-// const response = axios
-fetch("http://localhost:3000/items")
-.then((response) => {
-  // handle success
-  console.log(response);
-})
-.catch(function (error) {
-    // handle error
-    console.log(error);
-  });
+// @ts-ignore
+// import axios from "https://cdn.jsdelivr.net/npm/axios@1.6.7/+esm";
+let characters: any[] = [];
+async function fetchItems() {
+  try {
+    const response = await fetch("http://localhost:3000/items");
+    const data = await response.json();
+    //assign characters into data
+    characters = data;
+    console.log(characters[2]); // This is your actual data
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+// Calling the function
+fetchItems();
