b4a951f6d11c3d966fb334cfa4fa2662f0b517ae
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-09-01 08:58:17 +0200

    feat: added data to the card

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,57 @@
 // @ts-ignore
-// import axios from "https://cdn.jsdelivr.net/npm/axios@1.6.7/+esm";
-let characters: any[] = [];
+// import axios from "https://cdn.jsdelivr.net/npm/axios@1.6.7/+esm"
+interface Character {
+  id: string;
+  name: string;
+  ki: string;
+  MaxKi: string;
+  race: string;
+  gender: string;
+  description: string;
+  DeletedAt: null | String;
+  Affiliation: string;
+  image: String;
+}
+let characters: Character[] = [];
+let root = document.getElementById("root") as HTMLElement;
 async function fetchItems() {
   try {
     const response = await fetch("http://localhost:3000/items");
     const data = await response.json();
-    //assign characters into data
-    characters = data;
-    console.log(characters[3]); // This is your actual data
+    // console.log(data); // This is the actual data
+    characters = await data;
+    console.log(characters[0]);
+    createCard(root, characters[0]);
   } catch (error) {
     console.log("Error:", error);
   }
 }
 // Calling the function
 fetchItems();
-
-let root = document.getElementById("root") as HTMLElement;
+// console.log("hello");
+// console.log(characters);
 function noCharacterData(rootElement: HTMLElement, message: string): void {
   rootElement.innerHTML = message;
-};
-noCharacterData(root, "No character to display.");
+}
+// noCharacterData(root, "No character to display");
+function createCard(rootElement: HTMLElement, data: Character): void {
+  const card = document.createElement("div") as HTMLDivElement;
+  const card70 = document.createElement("div") as HTMLDivElement;
+  const card30 = document.createElement("div") as HTMLDivElement;
+  const idName = document.createElement("p") as HTMLDivElement;
+  const gender = document.createElement("p") as HTMLDivElement;
+  const race = document.createElement("p") as HTMLDivElement;
+  card.className = "card";
+  card70.className = "card70";
+  card30.className = "card30";
+  idName.textContent = ` Id: ${data.id} | Name: ${data.name}`;
+  gender.textContent = `Gander: ${data.gender}`;
+  race.textContent = `Race ${data.race}`;
+  card.append(card70, card30);
+  card30.append(idName, gender, race);
+  // const firstDiv = document.createElement("div") as HTMLDivElement;
+  // const secDiv = document.createElement("div") as HTMLDivElement;
+  // rootElement.appendChild(secDiv);
+  card70.style.backgroundImage = `url("https://dragonball-api.com/characters/goku_normal.webp")`;
+  rootElement.appendChild(card);
+} 
